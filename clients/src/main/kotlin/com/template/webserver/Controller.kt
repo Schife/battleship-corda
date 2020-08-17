3f2481d514becc1000ff3699057b81918121e9a3
lwogan <lorcan.wogan@r3.com>
2020-08-17 17:33:46 +0100

    endpoint cleanup and change shots to map of players

--- clients/src/main/kotlin/com/template/webserver/Controller.kt
+++ clients/src/main/kotlin/com/template/webserver/Controller.kt
@@ -2,8 +2,6 @@ package com.template.webserver
 
 
 import com.r3.battleship.flows.CreateGameFlow
-import com.r3.battleship.flows.GetGamesFlow
-import com.r3.battleship.schemas.GameDTO
 import net.corda.core.messaging.startFlow
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
@@ -11,14 +9,13 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import webserver.*
 import java.util.*
-import kotlin.collections.ArrayList
 import kotlin.collections.HashMap
 
 /**
  * Define your API endpoints here.
  */
 @RestController
-@RequestMapping("/") // The paths for HTTP requests are relative to this base path.
+@RequestMapping("/battleship") // The paths for HTTP requests are relative to this base path.
 class Controller(rpc: NodeRPCConnection) {
 
     companion object {
@@ -38,29 +35,20 @@ class Controller(rpc: NodeRPCConnection) {
     @PostMapping(value = ["/createGame"], produces = ["application/json"])
     private fun createGame(): ResponseEntity<Game> {
         val gameID = UUID.randomUUID().toString()
-        val players = listOf("player-1", "player-2", "player-3")
-        val sampleGame = Game(gameID,  players, true, false, GameStatus.UNSTARTED)
-        games[gameID] = sampleGame
-        return ResponseEntity<Game>(sampleGame, HttpStatus.OK)
-    }
-
-    @PostMapping(value = ["/test"], produces = ["application/json"])
-    private fun test(): ResponseEntity<Game> {
-        val gameID = UUID.randomUUID().toString()
         val ourIdentity = proxy.nodeInfo().legalIdentities.first().name.toString()
         val players = listOf(ourIdentity)
         val sampleGame = Game(gameID,  players, true, false, GameStatus.UNSTARTED)
+        games[gameID] = sampleGame
 
         var x =  proxy.startFlow(::CreateGameFlow, 4).returnValue.get()
 
         return ResponseEntity<Game>(sampleGame, HttpStatus.OK)
     }
 
-
-    @PostMapping(value = ["/joinGame"], produces = ["application/json"])
-    private fun joinGame(@RequestBody request: JoinGameRequest): ResponseEntity<Game> {
+    @PostMapping(value = ["{gameId]/joinGame"], produces = ["application/json"])
+    private fun joinGame(@PathVariable gameId:String): ResponseEntity<Game> {
         val ourIdentity = proxy.nodeInfo().legalIdentities.first().name.toString()
-        val game = games[request.id]!!
+        val game = games[gameId]!!
         game.players = game.players + ourIdentity
         game.isJoinable = false
         if (game.players.size == 4) {
@@ -69,22 +57,23 @@ class Controller(rpc: NodeRPCConnection) {
         return ResponseEntity<Game>(game, HttpStatus.OK)
     }
 
-    @PostMapping(value = ["/startGame"], produces = ["application/json"])
-    private fun startGame(@RequestBody request: StartGameRequest): ResponseEntity<Game> {
-        val game = games[request.id]!!
+    @PostMapping(value = ["/{gameId}/startGame"], produces = ["application/json"])
+    private fun startGame(@PathVariable gameId:String, @RequestBody request: StartGameRequest): ResponseEntity<Game> {
+        val game = games[gameId]!!
         game.status = GameStatus.STARTED
         return ResponseEntity<Game>(game, HttpStatus.OK)
     }
 
-    @PostMapping(value = ["/placeShip"], produces = ["application/json"])
-    private fun placeShip(@RequestBody placement: Placement): ResponseEntity<String> {
+    @PostMapping(value = ["/{gameId}/placeShip"], produces = ["application/json"])
+    private fun placeShip(@PathVariable gameId:String, @RequestBody placement: Placement): ResponseEntity<String> {
         return ResponseEntity("placed", HttpStatus.OK);
     }
 
-    @GetMapping(value = ["/gameState/{gameId}"], produces = ["application/json"])
+    @GetMapping(value = ["/{gameId}/gameState"], produces = ["application/json"])
     private fun getGameState(@PathVariable gameId:String): ResponseEntity<GameState> {
         var placement = Placement(Coordinate("3","2"), Coordinate("3","5"))
-        var gameState = GameState(placement, true, createPlayerStateList(), createShotList())
+        var identity = proxy.nodeInfo().legalIdentities.first().name.toString()
+        var gameState = GameState(placement, identity, true, createPlayerStateList(), createShotList())
 
         return ResponseEntity(gameState, HttpStatus.OK);
     }
@@ -99,26 +88,23 @@ class Controller(rpc: NodeRPCConnection) {
         return map;
     }
 
-    private fun createShotList(): HashMap<Coordinate, String> {
+    private fun createShotList(): HashMap<String, HashMap<Coordinate, String>> {
         val c = Coordinate("3","4")
         val c2 = Coordinate("3","5")
         val c3 = Coordinate("2","2")
         val c4 = Coordinate("3","4")
-        val c5 = Coordinate("1","7")
-        val c6 = Coordinate("2","7")
-        val c7 = Coordinate("7","3")
-        val c8 = Coordinate("8","8")
+
         val map = HashMap<Coordinate, String>()
         map.put(c, "HIT")
         map.put(c2, "HIT")
         map.put(c3, "MISS")
         map.put(c4, "MISS")
-        map.put(c5, "HIT")
-        map.put(c6, "HIT")
-        map.put(c7, "MISS")
-        map.put(c8, "MISS")
 
-        return map;
+        val playerMap = HashMap<String, HashMap<Coordinate, String>>()
+        playerMap.put("player1", map)
+        playerMap.put("player2", map)
+
+        return playerMap;
     }
 }
 
