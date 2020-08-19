503a86d7bb5cd5d28b0efa3b20e79419d538b459
Dimos Raptis <dimos.raptis@r3.com>
2020-08-19 11:06:47 +0100

    Hide loader on request error

--- clients/src/main/resources/static/app.js
+++ clients/src/main/resources/static/app.js
@@ -205,7 +205,8 @@ function placeShip(gameId, fromX, fromY, toX, toY) {
             hideLoader();
         },
         error: function(XMLHttpRequest, textStatus, errorThrown) {
-            alert("Attack could not be performed.");
+            hideLoader();
+            alert("Ship could not be placed.");
             console.log("Attack could not be performed.");
             console.log(JSON.stringify(errorThrown));
             console.log(textStatus);
@@ -483,6 +484,7 @@ function performAttack(gameId) {
                 hideLoader();
             },
             error: function(XMLHttpRequest, textStatus, errorThrown) {
+                hideLoader();
                 alert("Attack could not be performed.");
                 console.log("Attack could not be performed.");
                 console.log(errorThrown);
